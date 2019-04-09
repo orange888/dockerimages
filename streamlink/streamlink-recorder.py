40528b262d8eb87ee848b67a51b155f037aa7821
Liofal <lfa@gmx.com>
2019-04-09 20:16:13 +0200

    first working revision

--- streamlink/streamlink-recorder.py
+++ streamlink/streamlink-recorder.py
@@ -1,25 +1,110 @@
-#!/usr/bin/python
+# This script checks if a user on twitch is currently streaming and then records the stream via streamlink
+import json
+import subprocess
+import datetime
+import threading
+import argparse
+import httplib2
+import os
+import progressbar
+import re
 
-from subprocess import call
-import sys
-import time
+from urllib.request import urlopen
+from urllib.error import URLError
+from threading import Timer
 
-streamURL = str(sys.argv[1])
-streamQuality = str(sys.argv[2])
-streamName = str(sys.argv[3])
-streamOptions = str(sys.argv[4])
+from apiclient import discovery
+from googleapiclient.http import MediaFileUpload
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.clientsecrets import InvalidClientSecretsError
+from oauth2client.file import Storage
 
-streamOptions = streamOptions.split()
+# Init variables with some default values
+timer = 30
+user = ""
+quality = "best"
+is_recording = False
 
-streamBuilder = []
-streamBuilder.append("streamlink")
-for x in (0, len(streamOptions)-1):
-	streamBuilder.append(str(streamOptions[x]))
-streamBuilder.append(streamURL)
-streamBuilder.append(streamQuality)
-streamBuilder.append("-o")
-streamBuilder.append("/download/"+streamName+"-"+time.strftime("%Y%m%d%H%M%S")+".mkv")
+def check_user(user):
+    """ returns 0: online, 1: offline, 2: not found, 3: error """
+    global info
+    url = 'https://api.twitch.tv/kraken/streams/' + user + '?client_id=' + client_id
+    try:
+        info = json.loads(urlopen(url, timeout=15).read().decode('utf-8'))
+        if info['stream'] is None:
+            status = 1
+        else:
+            status = 0
+    except URLError as e:
+        if e.reason == 'Not Found' or e.reason == 'Unprocessable Entity':
+            status = 2
+        else:
+            status = 3
+    return status
 
-while True:
-	call(streamBuilder)
-	time.sleep(60)
+def loopcheck():
+    global is_recording
+    status = check_user(user)
+    if status == 2:
+        print("username not found. invalid username?")
+    elif status == 3:
+        print("unexpected error. maybe try again later")
+    elif status == 1:
+        t = Timer(timer, loopcheck)
+        print(user, "currently offline, checking again in", timer, "seconds")
+        t.start()
+    elif status == 0:
+        print(user, "online")
+
+        if not is_recording:
+            print("recording...")
+            is_recording = True
+            filename = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S") + " - " + user + " - " + (info['stream']).get("channel").get(
+                "status") + ".flv"
+            filename = re.sub('[^A-Za-z0-9. -\[\]@]+', '', filename)
+            subprocess.call(["streamlink", "https://twitch.tv/" + user, quality, "-o", "/download/"+filename])
+            print("Stream is done. Queuing upload if necessary and going back to checking..")
+            is_recording = False
+
+        else:
+            pass  # don't start a new recording if there is one running already
+
+        t = Timer(timer, loopcheck)
+        t.start()
+
+def main():
+    global timer
+    global user
+    global quality
+    global client_id
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-timer", help="Stream check interval (less than 15s are not recommended)")
+    parser.add_argument("-user", help="Twitch user that we are checking")
+    parser.add_argument("-quality", help="Recording quality")
+    parser.add_argument("-clientid", help="Your twitch app client id")
+    args = parser.parse_args()
+
+    if args.timer is not None:
+        timer = int(args.timer)
+    if args.user is not None:
+        user = args.user
+    if args.quality is not None:
+        quality = args.quality
+
+    if args.clientid is not None:
+        client_id = args.clientid
+    else:
+        print("Please create a twitch app and set the client id with -clientid [YOUR ID]")
+        return
+
+    t = Timer(timer, loopcheck)
+    print("Checking for", user, "every", timer, "seconds. Record with", quality, "quality.")
+    loopcheck()
+    t.start()
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
