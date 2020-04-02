d9f0e7e93709dc2c0fe9aeaf958da400991907d3
Liofal <lfa@gmx.com>
2020-04-02 08:24:05 +0200

    auto refresh images

---
+++ pullAll.sh
@@ -0,0 +1,4 @@
+../docker-compose -f jackett/docker-compose.yml pull
+../docker-compose -f transmission/docker-compose.yml pull
+../docker-compose -f sonarr/docker-compose.yml pull
+../docker-compose -f radarr/docker-compose.yml pull
---
+++ upAll.sh
@@ -0,0 +1,4 @@
+../docker-compose -f jackett/docker-compose.yml up -d
+../docker-compose -f transmission/docker-compose.yml up -d
+../docker-compose -f sonarr/docker-compose.yml up -d
+../docker-compose -f radarr/docker-compose.yml up -d
